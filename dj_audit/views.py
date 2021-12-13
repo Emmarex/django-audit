a5715135dae204d163b0dbf7bc08e8cec0e398ce
Fattyk <ogundele.fatai.k@gmail.com>
2021-12-13 12:53:54 +0100

    pagination added - with at most 100 items per page

--- dj_audit/views.py
+++ dj_audit/views.py
@@ -4,6 +4,7 @@ from django.db.models import Count, Q
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from dj_audit.models import AuditLog
 from dj_audit.settings import (API_BASE_URL, AUDIT_LOG_TEMPLATE,
@@ -24,6 +25,18 @@ def audit_log_page(request):
         query_filter['user__username'] = user_name
     #
     audit_logs = AuditLog.objects.filter(**query_filter)
+
+    # paginate audit logs begins here
+    page = request.GET.get('page', 1)
+    paginator = Paginator(audit_logs, 100) #100 items per page
+    try:
+        audit_logs = paginator.page(page)
+    except PageNotAnInteger:
+        audit_logs = paginator.page(1)
+    except EmptyPage:
+        audit_logs = paginator.page(paginator.num_pages)
+    # end of pagination
+
     context = {
         'users': get_user_model().objects.all(),
         'audit_logs': audit_logs
@@ -50,6 +63,17 @@ def request_status_page(request):
         annotate(no_success=no_success).annotate(
             no_failed=no_failed).annotate(no_not_found=no_not_found).order_by('path_info')
 
+    # paginate request_status begins here
+    page = request.GET.get('page', 1)
+    paginator = Paginator(api_break_down, 100) #100 items per page
+    try:
+        api_break_down = paginator.page(page)
+    except PageNotAnInteger:
+        api_break_down = paginator.page(1)
+    except EmptyPage:
+        api_break_down = paginator.page(paginator.num_pages)
+    # end of pagination
+    
     context = {
         'total_success': total_success,
         'total_not_found': total_not_found,
