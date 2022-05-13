7eb95db1509020a22310307a945992279d9c5b54
Oluwafemi Tairu <otairu@afexnigeria.com>
2022-05-13 10:17:32 +0100

    fix: user-agent char length

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def get_package_data(package):
 
 setup(
     name='dj_audit',
-    version='0.1.3',
+    version='0.1.4',
     author="Tairu Oluwafemi Emmanuel",
     author_email="tairuoluwafemi09@gmail.com",
     description="Django Audit is a simple Django app that tracks and logs requests to your application.",
